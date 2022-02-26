e75f78038f7a86906a563b0438fbf6f63b4d64b8
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-02-26 00:11:43 -0500

    fix indent and optimize zip code display method

--- src/App.js
+++ src/App.js
@@ -5,39 +5,34 @@ import "./style.css"
 
 
 export default function App() {
-
   const [items, setItems] = useState([])
   const [city, setCity] = useState("")
   let cityUppercase = city.toUpperCase()
-
-useEffect(() => {
-  console.log(`http://ctp-zip-api.herokuapp.com/city/${cityUppercase}`)
-  fetch(`http://ctp-zip-api.herokuapp.com/city/${cityUppercase}`)
+  
+  useEffect(() => {
+    console.log(`http://ctp-zip-api.herokuapp.com/city/${cityUppercase}`)
+    fetch(`http://ctp-zip-api.herokuapp.com/city/${cityUppercase}`)
     .then(res =>{
-              if (!res.ok) {
-                throw Error("error text")
-              }
-              return res.json()
+      if (!res.ok) {
+        throw Error("error text")
+      }
+      return res.json()
     }) 
     .then(rows => {
       setItems(rows)
     }).catch((error) => {
       console.log("Invalid City Name")
     })
-
-}, [city]) // Only render the values in [city] this list
-
-/*const displayCity = items.map(item => {
-  //console.log(item)
-  <p>Zip Code: {item} </p>
-})*/
+  }, [city]) // Only render the values in [city] this list
   
-const handleSubmit = (event) => {
-  event.preventDefault()
-  setCity(event.target.city.value)
-  //alert(`The city you enter: ${city}`)
-}
-
+  const displayCity = items.map(item => <p>Zip Code: {item} </p>)
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setCity(event.target.city.value)
+    //alert(`The city you enter: ${city}`)
+  }
+  
   return (
   <div>
     <nav>
@@ -64,17 +59,18 @@ const handleSubmit = (event) => {
       </div>
   <ul>
     <div className="box">
-
-        {items.map(item => (
+        {/*{items.map(item => (
 
           <li className="list">
             Zipcode: {item}
           </li>
-        ))}
-         </div>
+        ))}*/}
+        {displayCity}
+        </div>
     </ul>
   </div>
-  )}
+  )
+}
 
 
 
